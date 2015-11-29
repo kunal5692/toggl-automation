e61ea3b3728083a547fa653d1425131f13584afe
kunalcodes <techcraze5@gmail.com>
2015-11-29 12:29:40 +0530

    modified git.py

--- git.py
+++ git.py
@@ -13,11 +13,13 @@ import sys
 path=""
 cwd=""
 
-def getBranchName(result):
+def gitBranchName():
+	result = gitCommands("git status")
 	branch = result[0]
 	return branch[10:-1]
 
-def getCommmit(commit):
+def gitLog():
+	commit = gitCommands("git log")
 	head = commit[0]
 	return head[7:-1]
 
@@ -36,27 +38,33 @@ def gitCommands(command):
 	for i in output:
  		fobj.write(i)
 	fobj.close()
+	result = readGitChanges()
+	os.chdir(path)
+	return result
 
 def readGitChanges():
 	result =[]
-	with open('status.txt') as f:
+	with open('gitChanges.txt') as f:
 		for line in f:
 			#print line[:-1]
 			result.append(line)
 	os.chdir(path)
-
+	return result
 
 def main():
-	global path 
+	global path
 	path = raw_input("Please enter patht to your working git directory: ")
-	global cwd 
+	global cwd
 	cwd = os.getcwd()
-	os.chdir(path)	
-	gitCommands("ls")
-
+	branch = gitBranchName()
+	print "You are on branch: ",branch
+	head = gitLog()
+	print "the commit head is at: ",head
+	command = "git diff --name-only " + head
+	result = gitCommands(command)
+	print result
 	#branch = getBranchName(result)
 	#print("your workiong branch is: ",branch)
 
 if __name__ == '__main__':
 	main()
-
